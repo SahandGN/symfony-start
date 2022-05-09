119303f55e30594c2c76240d163f5cf5b1047598
SahandGN <sahand.gn@gmail.com>
2022-05-10 01:14:00 +0430

    add messages for contact us

---
+++ src/src/Entity/Message.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    /**
+     * @Assert\NotBlank
+     */
+    private $username;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    /**
+     * @Assert\Email
+     */
+    private $email;
+
+    #[ORM\Column(type: 'string', length: 511)]
+    /**
+     * @Assert\NotBlank
+     */
+    private $subject;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    /**
+     * @Assert\NotBlank
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
