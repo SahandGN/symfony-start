3aecada739028e8f8f75f2aee5243b1f0af7987c
SahandGN <sahand.gn@gmail.com>
2022-05-24 09:49:07 +0430

    Register and users list added

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,24 @@ class HomeController extends AbstractController
 {
     /**
      *
-     * @Route("/")
+     * @Route("/", name="home")
      * @return Response
      */
     public function index()
     {
 
         return $this->render('home/home.html.twig', []);
+    }
+
+    #[Route('/users',name: 'users_list')]
+    public function appUsers(EntityManagerInterface $entityManager): Response
+    {
+        $users = $entityManager
+            ->getRepository(User::class)
+            ->findAll();
+
+        return $this->render('home/users.html.twig', [
+            'users' => $users,
+        ]);
     }
 }
